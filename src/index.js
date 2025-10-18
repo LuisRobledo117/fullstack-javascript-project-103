f61d5bc4601f1f62453078322f79c028cb3904be
Luis Robledo <yanyirobledo@gmail.com>
2025-10-17 22:22:25 -0500

    Se realiza la instruccion 4 del proyecto, se instal la libraria lodash y se ejecuta la logica de la funcion gendiff para compara los archivos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import parseFile from './parsers.js';
+import _ from 'lodash';
 
 export default function genDiff(filepath1, filepath2) {
   const data1 = parseFile(filepath1);
   const data2 = parseFile(filepath2);
 
-  console.log('Archivo 1:', data1);
-  console.log('Archivo 2:', data2);
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  
+  const result = keys.map((key) => {
+    if(!Object.hasOwn(data2, key)) {
+      return ` - ${key}: ${data1[key]}`;
+    }
+    
+    if(!Object.hasOwn(data1, key)) {
+      return ` + ${key}: ${data2[key]}`;
+    }
 
-  return 'Archivos leídos correctamente';
-}
+    if(_.isEqual(data1[key], data2[key])){
+      return `  ${key}: ${data1[key]}`;
+    }
+
+      return ` - ${key}: ${data1[key]}\n + ${key}: ${data2[key]}`;
+  });
+
+  return `{\n${result.join('\n')}\n}`;
+};
