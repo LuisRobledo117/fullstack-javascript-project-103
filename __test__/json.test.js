9558548169313ebc7f3210759e05f81e1f7b1db8
Luis Robledo <yanyirobledo@gmail.com>
2025-11-10 20:46:32 -0500

    se corrigen los errores en las pruebas

--- __test__/json.test.js
+++ __test__/json.test.js
@@ -10,7 +10,7 @@ const dirName = path.dirname(fileName);
 const getFixturePath = (filename) => path.join(dirName, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const normalize = (str) => str.replace(/\r\n/g, '\n');
+const normalize = (str) => str.replace(/\n/g, '\r\n');
 
 describe('json formatter', () => {
   test('genDiff JSON Anidado', () => {
