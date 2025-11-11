aeb7edd17665ee61c52f8fdccf7a566805091fad
Luis Robledo <yanyirobledo@gmail.com>
2025-11-10 21:54:26 -0500

    se corrigen errores en las pruebas con los archivos .yml

--- __test__/gendiff.test.js
+++ __test__/gendiff.test.js
@@ -10,7 +10,7 @@ const dirName = path.dirname(fileName);
 const getFixturePath = (filename) => path.join(dirName, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('gendiff yml', () => {
+test('gendiff json', () => {
   const filepath1 = getFixturePath('file1.json');
   const filepath2 = getFixturePath('file2.json');
   const stylishResult = readFile('expected_stylish.txt');
@@ -32,11 +32,9 @@ test('gendiff yml', () => {
   const plainResult = readFile('expected_plain.txt');
   const jsonResult = readFile('expected_json.txt');
 
-  const normalize = (text) => text.replace(/\r\n/g, '\n');
-
-  expect(normalize(genDiff(filepath1, filepath2))).toEqual(normalize(stylishResult));
-  expect(normalize(genDiff(filepath1, filepath2, 'stylish'))).toEqual(normalize(stylishResult));
-  expect(normalize(genDiff(filepath1, filepath2, 'plain'))).toEqual(normalize(plainResult));
-  expect(normalize(genDiff(filepath1, filepath2, 'json'))).toEqual(normalize(jsonResult));
+  expect(genDiff(filepath1, filepath2)).toEqual(stylishResult);
+  expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(stylishResult);
+  expect(genDiff(filepath1, filepath2, 'plain')).toEqual(plainResult);
+  expect(genDiff(filepath1, filepath2, 'json')).toEqual(jsonResult);
 });
 
